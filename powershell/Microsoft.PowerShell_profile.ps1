ed905f4358b78cd8564fc35037882755450d8042
Alex Lewis <alex.lewis@synthace.com>
2023-05-26 13:57:26 +0100

    Work powershell config

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -40,14 +40,59 @@ function init_analyzer() {
   Install-Module -Name PSScriptAnalyzer -Force
 }
 
-init_posh_git
+function init_syn_cloud_vm() {
+  $path = "$HOME\Repos\SynthCloudVM-PowerShell\SynthCloudVM"
+  Import-Module $path
+  Push-Location $path
+  $version = $(git id)
+  $branch = $(git rev-parse --abbrev-ref HEAD)
+  Pop-Location
+  return @($branch, $version)
+}
 
-Write-Host -ForegroundColor Green "## LexerConfig v1.0.2 ##"
+function StartAlexVM() {
+  param(
+      [Parameter(Mandatory=$false)][string]$vmName
+  )
+  $started = $false
+  try {
+    # if no VM name was passed offer a list of choices because no-one likes typing out VM names
+    if (!$vmName) {
+      $vms = Get-SynthCloudVM -owner "alex.lewis@synthace.com"
+      $i = 0
+      foreach ($vm in $vms) {
+        Write-Host -ForegroundColor Cyan "$i. " -NoNewline
+        Write-Host -ForegroundColor Green "$($vm.Name)"
+        $i++
+      }
+      $choice = Read-Host -Prompt "Please select a VM: "
+      $vmName = $vms[$choice].Name
+      Write-Host "Starting `"$vmName`""
+    }
+    $started = $true
+    Start-SynthCloudVM $vmName
+    $job = Start-Job -ScriptBlock { Start-Sleep -Seconds 1; & "$env:WINDIR\system32\mstsc.exe" /v:localhost:3390 }
+    Start-SynthCloudVMTunnel $vmName
+  } finally {
+    if ($started) {
+      Stop-Job -Job $job
+      Stop-SynthCloudVM $vmName
+    }
+  }
+}
+
+init_posh_git
+$info = init_syn_cloud_vm
+
+Write-Host -ForegroundColor Green "## Lexer WorkConfig v1.0.2 ##"
 
 Write-Host "History location " -NoNewLine
 Write-Host -ForegroundColor Green "$($(Get-PSReadlineOption).HistorySavePath)"
 
 Write-Host "Powershell Profile loaded " -NoNewLine
 Write-Host -ForegroundColor Green "$profile"
+
+Write-Host "SynthCloudVM " -NoNewLine
+Write-Host -ForegroundColor Green "$($info[0]) @ $($info[1])"
 
 Write-Host "==============================================================================================="
